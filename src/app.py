fd8e7988488185421675f97e8fdb991e03692dc1
sim3075 <simonmiranda30dic@gmail.com>
2024-11-01 01:52:41 -0500

    redeploy

--- src/app.py
+++ src/app.py
@@ -3,21 +3,18 @@ from routes.continuous_distributions import continuous
 from routes.discrete_distributions import discrete
 from routes.hypothesis_test import hypothesis
 
-def createApp():
-    app = Flask(__name__)
+
+app = Flask(__name__)
 
 
-    @app.route('/')
-    def index():
-        return render_template('index.html')
+@app.route('/')
+def index():
+    return render_template('index.html')
     
+app.register_blueprint(continuous)
+app.register_blueprint(discrete)
+app.register_blueprint(hypothesis)
+app.add_url_rule('/', endpoint='index')
 
-    app.register_blueprint(continuous)
-
-    app.register_blueprint(discrete)
-
-    app.register_blueprint(hypothesis)
-
-    app.add_url_rule('/', endpoint='index')
-
-    return app
+if __name__ == '__main__':
+    app.run(debug=False)
